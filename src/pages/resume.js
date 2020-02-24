0c6805b7c55b6a3383dcd6b7a9aa5c3f10746c75
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-02-24 17:51:33 -0500

    updated page titles

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -7,10 +7,10 @@ import SEO from "../components/seo"
 const ResumePage = props => {
   return (
     <Layout>
-      <SEO title="Page two" />
+      <SEO title="Resume" />
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
-      <Link to="/">Go back to the homepage</Link>
+      
     </Layout>
   )
 }
