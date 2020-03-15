b9646517bafda12832eeefa341f02b0a6ba4c5c1
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-14 20:18:31 -0400

    added in image to photos

--- src/components/About/Photos.js
+++ src/components/About/Photos.js
@@ -5,15 +5,24 @@ import Img from "gatsby-image"
 const Section = styled.section`
   text-align: center;
   background-color: #ffffff;
-  padding: 30px 20px;
+  padding: 0 20px;
   width: 100%;
   position: relative;
 `
+const Photo = styled(Img)`
+  height: 280px;
+  width: 280px;
+  margin: 0 auto;
+`
 
 const PhotoSection = props => {
-    return (
-      <Section>
-      </Section>
-    )
-  }
-  export default PhotoSection;
+  return (
+    <Section>
+      <Photo
+        fluid={props.data.amsterdamImg.childImageSharp.fluid}
+        role="image"
+      />
+    </Section>
+  )
+}
+export default PhotoSection
