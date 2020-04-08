d031657ea9184043f6fb732dd52eebc64849c8fa
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-04-08 12:01:19 -0400

    updated fonts for tech.js

--- src/components/About/Tech.js
+++ src/components/About/Tech.js
@@ -10,33 +10,43 @@ const Section = styled.section`
   h2 {
     font-family: "Raleway", sans-serif;
     font-weight: 400;
+    font-size: 24px;
+    line-height: 28px;
     margin: 1em auto;
   }
   p {
     font-family: "Raleway", sans-serif;
     font-weight: 300;
     margin: 0.5em 0;
-    font-size: 18px;
-    line-height: 22px;
+    font-size: 20px;
+    line-height: 28px;
   }
   span {
     font-weight: 500;
   }
 `
 
+const Bold = styled.h4`
+  font-family: "Raleway", sans-serif;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 28px;
+`
+
 const Tech = props => {
   return (
     <Section>
       <h2>Tech Stack</h2>
-      <p>
-        <span>Editor:</span> VS Code
-      </p>
-      <p>
-        <span>Front-End:</span> HTML, CSS, React, Javascript ES6, RESTful APIs, GraphQL
-      </p>
+      <Bold>Editor:</Bold>
+      <p>VS Code</p>
+      <Bold>Front-End:</Bold>
+      <p>HTML, CSS, React, Javascript ES6, RESTful APIs, GraphQL</p>
       <p>
         <span>Backend:</span> MySQL, MongoDB, Node.js, Python, Django, Express,
         Mongoose, PostgreSQL
+      </p>
+      <p>
+        <span>Adobe Creative Suite:</span>Adobe XD, Photoshop, Illustrator
       </p>
     </Section>
   )
